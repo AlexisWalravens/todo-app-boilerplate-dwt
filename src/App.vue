f329f351701918e7a879079be7a5e33f6724012d
Alexis Walravens <alexiswalravens@gmail.com>
2021-10-20 12:57:49 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <h1>Vue Todo List</h1>
+    <VTitle>Vue Todo List</VTitle>
+
+    <VTodoAdd />
+
+    <ul class="todo-list">
+      <VTodoItem />
+    </ul>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VTitle from './components/VTitle'
+import VTodoAdd from './components/VTodoAdd'
+import VTodoItem from './components/VTodoItem'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VTodoItem,
+    VTodoAdd,
+    VTitle
+  },
+
+  setup () {
+    return {}
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +42,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+.todo-list {
+  background: #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 }
 </style>
