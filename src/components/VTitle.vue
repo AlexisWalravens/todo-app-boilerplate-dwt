697e898202447f41f5dc282733f1ae067cfa1f2f
Alexis Walravens <alexiswalravens@gmail.com>
2021-10-20 20:22:07 +0200

    feat: Add style to components

--- src/components/VTitle.vue
+++ src/components/VTitle.vue
@@ -11,5 +11,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.v-title {
+  font-size: 48px;
+}
 </style>
--- src/components/VTodoItem.vue
+++ src/components/VTodoItem.vue
@@ -13,5 +13,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.v-todo-item {
+  padding: 8px;
+  display: flex;
+  justify-content: space-between;
+  background: white;
+  border-radius: 3px;
+  background: antiquewhite;
 
+  &.is-checked {
+    filter: grayscale(1);
+
+    span {
+      text-decoration: line-through;
+    }
+  }
+}
 </style>
